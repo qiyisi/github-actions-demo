d9aa219093f83516464fea628eef1adda6a417f9
qiyisi <qiyisi@qq.com>
2021-01-22 13:51:37 +0800

    Update

--- site/src/App.js
+++ site/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{ process.env.REACT_APP_NAME }</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
